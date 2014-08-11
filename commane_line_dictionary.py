3950653029e76d4a25329d5a7a137dcd11066ffa
omkarvijay5 <omkarvijay5@gmail.com>
2014-08-11 15:09:04 +0530

    added initial setup for the wordnik api

---
+++ commane_line_dictionary.py
@@ -0,0 +1,10 @@
+import os
+from wordnik import *
+
+
+class CommandLineDict(object):
+
+    def __init__(self):
+        apiUrl = 'http://api.wordnik.com/v4'
+        apiKey = os.environ['API_KEY']
+        self.client = swagger.ApiClient(apiKey, apiUrl)
