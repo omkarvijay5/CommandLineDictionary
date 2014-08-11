92da500c16b8444fb6bc23e28e56a1acbb11d29f
omkarvijay5 <omkarvijay5@gmail.com>
2014-08-11 15:19:18 +0530

    initial commit for get_definitions with no implementation

--- commane_line_dictionary.py
+++ commane_line_dictionary.py
@@ -1,5 +1,6 @@
 import os
 from wordnik import *
+import sys
 
 
 class CommandLineDict(object):
@@ -8,3 +9,12 @@ class CommandLineDict(object):
         apiUrl = 'http://api.wordnik.com/v4'
         apiKey = os.environ['API_KEY']
         self.client = swagger.ApiClient(apiKey, apiUrl)
+
+    def get_definitions(self):
+        pass
+
+
+if __name__ == '__main__':
+    class_object = CommandLineDict()   
+    if sys.argv[1] == 'def':
+        class_object.get_definitions()
