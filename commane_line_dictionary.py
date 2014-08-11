78a6737d03a613e24f34fee66a82581e65e4d70f
omkarvijay5 <omkarvijay5@gmail.com>
2014-08-11 15:33:20 +0530

    added get definitions functionality

--- commane_line_dictionary.py
+++ commane_line_dictionary.py
@@ -10,11 +10,15 @@ class CommandLineDict(object):
         apiKey = os.environ['API_KEY']
         self.client = swagger.ApiClient(apiKey, apiUrl)
 
-    def get_definitions(self):
-        pass
+    def get_definitions(self, word):
+        word_api = WordApi.WordApi(self.client)
+        definitions = word_api.getDefinitions(word, limit=50)
+        for definition in definitions:
+            print '* %s' % definition.text
 
 
 if __name__ == '__main__':
-    class_object = CommandLineDict()   
+    class_object = CommandLineDict()
     if sys.argv[1] == 'def':
-        class_object.get_definitions()
+        word = sys.argv[2]
+        class_object.get_definitions(word)
