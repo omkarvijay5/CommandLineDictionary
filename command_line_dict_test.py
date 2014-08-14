f12a546adae16e57afc4d743a7a9b9bac2b19cd8
omkarvijay5 <omkarvijay5@gmail.com>
2014-08-14 13:00:36 +0530

    added setup for testing

--- command_line_dict_test.py
+++ command_line_dict_test.py
@@ -1,10 +1,16 @@
 import unittest
 
 from command_line_dictionary import CommandLineDict
+from wordnik import *
+from wordnik.WordsApi import *
 
 
 class TestCommandLineDict(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        apiUrl = 'http://api.wordnik.com/v4'
+        apiKey = os.environ['API_KEY']
+        self.client = swagger.ApiClient(apiKey, apiUrl)
 
 
 if __name__ == '__main__':
