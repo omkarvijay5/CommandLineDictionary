997f336d4859de18d570cf1fdabfa533553328ae
omkarvijay5 <omkarvijay5@gmail.com>
2014-08-14 13:17:52 +0530

    added test for get_synonyms

--- command_line_dict_test.py
+++ command_line_dict_test.py
@@ -11,7 +11,12 @@ class TestCommandLineDict(unittest.TestCase):
         apiUrl = 'http://api.wordnik.com/v4'
         apiKey = os.environ['API_KEY']
         self.client = swagger.ApiClient(apiKey, apiUrl)
+        self.test_object = CommandLineDict()
 
+    def test_get_synonyms(self):
+        word = 'hello'
+        synonyms = self.test_object.get_synonyms(word)
+        self.assertEqual(len(synonyms), 10)
 
 if __name__ == '__main__':
     unittest.main()
