9856c2c1f75be5217dfcfd88fcfe034d2eaeeb0b
omkarvijay5 <omkarvijay5@gmail.com>
2014-08-14 12:51:35 +0530

    initial commit for testing command line dict

---
+++ command_line_dict_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from command_line_dictionary import CommandLineDict
+
+
+class TestCommandLineDict(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
