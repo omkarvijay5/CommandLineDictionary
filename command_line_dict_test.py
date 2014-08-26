285238d2699969dac1807d075ceb741695fe3688
omkarvijay5 <omkarvijay5@gmail.com>
2014-08-26 18:45:05 +0530

    added test cases for get antonyms

--- command_line_dict_test.py
+++ command_line_dict_test.py
@@ -6,6 +6,7 @@ from wordnik import *
 from command_line_dictionary import CommandLineDict
 from mock import patch
 import wordnik
+import pdb
 
 
 class TestCommandLineDict(unittest.TestCase):
@@ -40,7 +41,17 @@ class TestCommandLineDict(unittest.TestCase):
                   'test1', 'test2', 'test3']
         self.assertEqual(original_call, result)
 
-
+    @patch('wordnik.WordApi.WordApi')
+    def test_get_antonyms(self, mock_object):
+        instance = mock_object.return_value
+        mock = MagicMock()
+        mock.relationshipType = 'antonym'
+        mock.words = ['test1', 'test2']
+        instance.getRelatedWords.return_value = [mock, mock, mock]
+        original_call = self.test_object.get_antonyms('test')
+        self.assertEqual(len(original_call), 6)
+        result = ['test1', 'test2', 'test1', 'test2', 'test1', 'test2']
+        self.assertEqual(result, original_call)
 
 if __name__ == '__main__':
     unittest.main()
