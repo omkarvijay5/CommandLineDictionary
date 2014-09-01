9f56d0a44237b4bc14b4f5ada13dda3a58cb2ced
omkarvijay5 <omkarvijay5@gmail.com>
2014-09-01 16:55:53 +0530

    refactored test code and added reusable methods for synonyms antonyms definitions and get examples

--- command_line_dict_test.py
+++ command_line_dict_test.py
@@ -9,7 +9,6 @@ from StringIO import StringIO
 from command_line_dictionary import CommandLineDict
 
 
-
 class TestCommandLineDict(unittest.TestCase):
 
     def setUp(self):
@@ -22,54 +21,105 @@ class TestCommandLineDict(unittest.TestCase):
     def test_get_definitions(self, mock_object):
         instance = mock_object.return_value
         mock = MagicMock()
-        mock.text = 1
-        instance.getDefinitions.return_value = [mock, mock, mock]
-        original_call = self.test_object.get_definitions(mock)
-        self.assertEqual(len(instance.getDefinitions()), len(original_call))
-        self.assertEqual(original_call, [1, 1, 1])
+        self.set_mock_and_test_definitions(mock, instance)
 
     @patch('wordnik.WordApi.WordApi')
     def test_get_synonyms(self, mock_object):
         instance = mock_object.return_value
         mock = MagicMock()
+        self.set_mock_and_test_synonyms(mock, instance)
+
+    @patch('wordnik.WordApi.WordApi')
+    def test_get_antonyms(self, mock_object):
+        instance = mock_object.return_value
+        mock = MagicMock()
+        self.set_mock_and_test_antonyms(mock, instance)
+
+    @patch('wordnik.WordApi.WordApi')
+    def test_get_example(self, mock_object):
+        instance = mock_object.return_value
+        mock = MagicMock()
+        self.set_mock_and_test_examples(mock, instance)
+
+    @patch('wordnik.WordApi.WordApi')
+    def test_get_dictionary(self, mock_object):
+        instance = mock_object.return_value
+        mock = MagicMock()
+        [mock, instance] = self.set_mock_and_test_antonyms(mock, instance)
+        [mock, instance] = self.set_mock_and_test_synonyms(mock, instance)
+        [mock, instance] = self.set_mock_and_test_definitions(mock, instance)
+        [mock, instance] = self.set_mock_and_test_examples(mock, instance)
+        saved_stdout = sys.stdout
+        out = StringIO()
+        sys.stdout = out
+        original_call = self.test_object.get_dictionary('test_word')
+        output = out.getvalue().strip()
+        result = ("Examples of the word test_word are"
+                  "\n* test1\n* test1\n* test1\nSynonyms of the word test_word"
+                  " are\ntest1\ntest2\ntest3\ntest1\ntest2\ntest3\ntest1"
+                  "\ntest2\ntest3\nNo Antonyms for the word test_word\n"
+                  "Definitions of the word test_word are\ntest1\ntest1\ntest1")
+        self.assertEqual(output, result)
+        self.set_mock_and_test_antonyms(mock, instance)
+        original_call = self.test_object.get_dictionary('test_word')
+        output = out.getvalue().strip()
+        result = ("Examples of the word test_word are"
+                  "\n* test1\n* test1\n* test1\nSynonyms of the word test_word"
+                  " are\ntest1\ntest2\ntest3\ntest1\ntest2\ntest3\ntest1"
+                  "\ntest2\ntest3\nNo Antonyms for the word test_word\n"
+                  "Definitions of the word test_word are\ntest1\ntest1\ntest1"
+                  "\nExamples of the word test_word are\n* test1\n* test1\n*"
+                  " test1\nNo Synonyms for the word test_word\nAntonyms of the"
+                  " word test_word are\ntest1\ntest2\ntest1\ntest2\ntest1\n"
+                  "test2\nDefinitions of the word test_word are\ntest1\ntest1"
+                  "\ntest1")
+        self.assertEqual(output, result)
+        sys.stdout = saved_stdout
+
+    def set_mock_and_test_antonyms(self, mock, method):
+        mock.relationshipType = 'antonym'
+        mock.words = ['test1', 'test2']
+        method.getRelatedWords.return_value = [mock, mock, mock]
+        original_call = self.test_object.get_antonyms('test')
+        self.assertEqual(len(original_call), 6)
+        result = ['test1', 'test2', 'test1', 'test2', 'test1', 'test2']
+        self.assertEqual(result, original_call)
+        return [mock, method]
+
+    def set_mock_and_test_synonyms(self, mock, method):
         mock.relationshipType = "synonym"
         mock.words = ['Test1', 'Test2', 'Test3']
-        instance.getRelatedWords.return_value = [mock, mock, mock]
+        method.getRelatedWords.return_value = [mock, mock, mock]
         original_call = self.test_object.get_synonyms('test')
         self.assertEqual(len(original_call), 9)
         result = ['test1', 'test2', 'test3',
                   'test1', 'test2', 'test3',
                   'test1', 'test2', 'test3']
         self.assertEqual(original_call, result)
+        return [mock, method]
 
-    @patch('wordnik.WordApi.WordApi')
-    def test_get_antonyms(self, mock_object):
-        instance = mock_object.return_value
-        mock = MagicMock()
-        mock.relationshipType = 'antonym'
-        mock.words = ['test1', 'test2']
-        instance.getRelatedWords.return_value = [mock, mock, mock]
-        original_call = self.test_object.get_antonyms('test')
-        self.assertEqual(len(original_call), 6)
-        result = ['test1', 'test2', 'test1', 'test2', 'test1', 'test2']
-        self.assertEqual(result, original_call)
+    def set_mock_and_test_definitions(self, mock, method):
+        mock.text = 1
+        method.getDefinitions.return_value = [mock, mock, mock]
+        original_call = self.test_object.get_definitions(mock)
+        self.assertEqual(len(method.getDefinitions()), len(original_call))
+        self.assertEqual(original_call, [1, 1, 1])
+        return [mock, method]
 
-    @patch('wordnik.WordApi.WordApi')
-    def test_get_example(self, mock_object):
-        instance = mock_object.return_value
-        mock = MagicMock()
+    def set_mock_and_test_examples(self, mock, method):
         mock.text = 'test1'
+        method.getExamples.return_value = mock
         mock.examples = [mock, mock, mock]
-        instance.getExamples.return_value = mock
         saved_stdout = sys.stdout
-        try:
-            out = StringIO()
-            sys.stdout = out
-            original_call = self.test_object.get_examples('test_word')
-            output = out.getvalue().strip()
-            self.assertEqual(output, 'Examples of the word test_word are\n* test1\n* test1\n* test1')
-        finally:
-            sys.stdout=saved_stdout
+        out = StringIO()
+        sys.stdout = out
+        original_call = self.test_object.get_examples('test_word')
+        output = out.getvalue().strip()
+        result = ("Examples of the word test_word are\n* test1\n* test1\n*"
+                  " test1")
+        self.assertEqual(output, result)
+        sys.stdout = saved_stdout
+        return [mock, method]
 
 if __name__ == '__main__':
     unittest.main()
